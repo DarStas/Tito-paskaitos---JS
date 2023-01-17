62858703fd5c6c96cd70c8fe8c7205c8872c91f5
DarsStas <darstas@yahoo.com>
2023-01-17 20:02:06 +0200

    arrays 01-17

---
+++ 2023-01-10/arrays.js
@@ -0,0 +1,14 @@
+const favoriteNumber1 = 1;
+const favoriteNumber2 = 4;
+const favoriteNumber3 = 76;
+const favoriteNumber4 = 18;
+
+// const favoriteNumbers = `${favoriteNumber1} ${favoriteNumber2} ${favoriteNumber3}`;
+
+// console.log(favoriteNumbers);
+
+const favoriteNumbers = [favoriteNumber1, favoriteNumber2, favoriteNumber3];
+
+console.log(favoriteNumbers);
+
+const arraySample = ['some text', 1, {text: 'abs'}, () => {}, true, undefined, null];
--- 2023-01-10/script.js
+++ 2023-01-10/script.js
@@ -25,3 +25,41 @@ console.log(baseObjViaClass);
 const testFunc = (arg1, arg2) => {}; // arrow function taip užsirašo
 
 testFunc()
+
+// --------- BONUS Factorial -------- 
+// while ciklo panaudojimas
+// do...while ciklo panaudojimas
+// for ciklo panaudojimas
+
+const getNewFactorial = (number) => {
+    let result = 1;
+    while (number > 1) {
+        result *= number;
+        number--;
+    }
+    return result
+}
+
+console.log(getNewFactorial(1));
+
+const getOtherFactorial = (number) => {
+    let result = 1;
+    do {
+        result *= number;
+        number--;
+    } while (number > 1);
+    return result;
+}
+
+console.log(getOtherFactorial(6));
+
+
+const getNaujausiasFactorial = (number) => {
+    let result = 1;
+    for (let i = 1; i <= number; i++) {
+        result *= i;
+    }
+    return result
+}
+
+console.log(getNaujausiasFactorial(3));
